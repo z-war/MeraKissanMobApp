91a225a52a76d57034453a9ffbe985293aea1c47
z-war <zawarhassan112@gmail.com>
2020-05-05 17:22:00 +0500

    sell screen xml comlpeted

--- app/src/main/java/com/example/merakissan/Fragments/sell_fragment.java
+++ app/src/main/java/com/example/merakissan/Fragments/sell_fragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -17,9 +18,11 @@ import com.example.merakissan.R;
 public class sell_fragment extends Fragment {
 
     private View frag;
-    private Spinner dropdown;
-    private String[] items = {"Cattle", "Machinery"};
-    private ArrayAdapter<String> adapter;
+    private Spinner dropdown , Dropdown_cat;
+    private String[] items = {"LiveStock", "Equipment"};
+    private String[] items_cattle = {"Cow" ,"Buffalo", "Sheep"  , "Goat" ,"Poultry" , "Camel" };
+    private String[] items_equipment = {"Tractor" , "Cultivator" , "Plower" , "harrows" , "Sprayers" ,"Seed Drills" ,"Scythe" ,"Sickle","Harvester"};
+    private ArrayAdapter<String> adapter ,adapter_for_cateory;
 
     public sell_fragment() {
         // Required empty public constructor
@@ -35,14 +38,50 @@ public class sell_fragment extends Fragment {
 
     private void init() {
         try {
+            Dropdown_cat = frag.findViewById(R.id.product_category);
+            adapter_for_cateory = new ArrayAdapter<>(getActivity().getBaseContext(),android.R.layout.simple_spinner_dropdown_item,items_cattle);
+            Dropdown_cat.setAdapter(adapter_for_cateory);
             dropdown = frag.findViewById(R.id.product_type);
             adapter = new ArrayAdapter<>( getActivity().getBaseContext(),android.R.layout.simple_spinner_dropdown_item,items);
             dropdown.setAdapter(adapter);
+            dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    switch (position){
+                        case 0:
+                            adapter_for_cateory = new ArrayAdapter<>(getActivity().getBaseContext(),android.R.layout.simple_spinner_dropdown_item,items_cattle);
+                            Dropdown_cat.setAdapter(adapter_for_cateory);
+                            break;
+                        case 1:
+                            adapter_for_cateory = new ArrayAdapter<>(getActivity().getBaseContext(),android.R.layout.simple_spinner_dropdown_item,items_equipment);
+                            Dropdown_cat.setAdapter(adapter_for_cateory);
+
+                            break;
+
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
         }catch (Exception e)
         {
             Toast.makeText(getContext(), "Error "+e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
     }
+
+    private void change_categries()
+    {
+        try {
+
+        }catch (Exception e)
+        {
+            Toast.makeText(getContext(), "change_categries error "+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 }
